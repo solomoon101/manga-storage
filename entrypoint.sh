d9c34206e256928d7f3020fe03dd6e5f04630bbd
Eltrach <aeltrach@gmail.com>
2025-04-19 20:18:19 +0100

    refactor: simplify environment variable substitution in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 set -e
 
-# Define a list of variables to substitute
-export NGINX_VARS='${NGINX_PORT} ${SERVER_NAME} ${ROOT_PATH}'
-
-# Substitute environment variables in the template
-# Use eval to expand NGINX_VARS correctly for envsubst
-eval "envsubst \"$NGINX_VARS\" < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf"
-
+# Substitute listed environment variables in the template
+# The single quotes prevent shell expansion here, envsubst handles the ${VAR} syntax
+envsubst '${NGINX_PORT} ${SERVER_NAME} ${ROOT_PATH}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Execute the CMD passed to the container (e.g., nginx -g 'daemon off;')
 exec "$@" 
